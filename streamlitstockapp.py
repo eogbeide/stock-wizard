fd1bc6780e37f6a0ab5bbe943853764e1ec7881d
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-09 09:41:04 -0800

    Update streamlitstockapp.py

--- streamlitstockapp.py
+++ streamlitstockapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import plotly.express as px
-from prophet import Prophet
+from fbprophet import Prophet
 from plotly import graph_objs as go
 import glob
 
