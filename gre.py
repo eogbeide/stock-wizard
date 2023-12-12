54a6337f11ac507deb176501580f682fc3cd09a8
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-12 09:45:47 -0800

    Update gre.py

--- gre.py
+++ gre.py
@@ -1,30 +1,31 @@
 import streamlit as st
 import pandas as pd
 
-# Open the CSV file and read its contents
-with open('top_500_gre_words.csv', 'r', encoding='utf-8-sig') as file:
-    try:
-        words_df = pd.read_csv(file)
-    except UnicodeDecodeError:
-        st.error("Error: Unable to decode the CSV file. Please check the file's encoding.")
-
-# Create a dictionary mapping words to meanings and example sentences
-words_dict = {
-    row['Word']: {'Meaning': row['Meaning'], 'Example': row['Example']}
-    for _, row in words_df.iterrows()
-}
-
 # Streamlit app
 def main():
     st.title("Top 500 GRE Words")
     st.write("Choose a word to get its meaning and an example sentence.")
 
-    # Select a word from the dropdown
-    selected_word = st.selectbox("Select a word", list(words_dict.keys()))
+    try:
+        # Open the CSV file and read its contents
+        with open('top_500_gre_words.csv', 'r', encoding='utf-8-sig') as file:
+            words_df = pd.read_csv(file)
 
-    # Display the meaning and example sentence
-    st.write(f"Meaning: {words_dict[selected_word]['Meaning']}")
-    st.write(f"Example: {words_dict[selected_word]['Example']}")
+        # Create a dictionary mapping words to meanings and example sentences
+        words_dict = {
+            row['Word']: {'Meaning': row['Meaning'], 'Example': row['Example']}
+            for _, row in words_df.iterrows()
+        }
+
+        # Select a word from the dropdown
+        selected_word = st.selectbox("Select a word", list(words_dict.keys()))
+
+        # Display the meaning and example sentence
+        st.write(f"Meaning: {words_dict[selected_word]['Meaning']}")
+        st.write(f"Example: {words_dict[selected_word]['Example']}")
+
+    except UnicodeDecodeError:
+        st.error("Error: Unable to decode the CSV file. Please check the file's encoding.")
 
 if __name__ == '__main__':
     main()
