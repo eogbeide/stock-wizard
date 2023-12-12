edf5aaf98298ae57023b058d5d4b5bb04e6025ad
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-12 12:39:59 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from prophet import Prophet
+from plotly import graph_objs as go
 import glob
+
 from pandas_datareader import data as pdr
 from datetime import date
 import yfinance as yf
@@ -11,7 +13,7 @@ import os
 yf.pdr_override()
 
 # Tickers list
-ticker_list = ['INAB', 'CCCC', 'CADL', 'ADTX', 'MTCH', 'EA', 'PYPL', 'INTC', 'PFE', 'MRNA', 'VWAPY', 'CRL', 'CRM', 'AFRM', 'MU', 'AMAT', 'DELL', 'HPQ', 'BABA', 'VTWG', 'SPGI', 'STX', 'LABU', 'TSM', 'AMZN', 'BOX', 'AAPL', 'NFLX', 'AMD', 'GME', 'GOOG', 'GUSH', 'LU', 'META', 'MSFT', 'NVDA', 'PLTR', 'SITM', 'SPCE', 'SPY', 'TSLA', 'URI', 'WDC']
+ticker_list = ['INAB','CCCC','CADL','ADTX', 'MTCH', 'EA', 'PYPL', 'INTC', 'PFE', 'MRNA', 'VWAPY', 'CRL', 'CRM', 'AFRM', 'MU', 'AMAT', 'DELL', 'HPQ', 'BABA', 'VTWG', 'SPGI', 'STX', 'LABU', 'TSM', 'AMZN', 'BOX', 'AAPL', 'NFLX', 'AMD', 'GME', 'GOOG', 'GUSH', 'LU', 'META', 'MSFT', 'NVDA', 'PLTR', 'SITM', 'SPCE', 'SPY', 'TSLA', 'URI', 'WDC']
 today = date.today()
 
 # We can get data by our choice by giving days bracket
@@ -38,23 +40,26 @@ for tik in ticker_list:
     getData(tik)
 
 # Pull data, train model, and predict
-def select_file(files):
+def select_files(files):
     num_files = len(files)
 
-    while True:
-        try:
-            choice = st.sidebar.selectbox(
-                "Select Company Ticker",
-                range(1, num_files + 1),
-                format_func=lambda x: files[x - 1].split('/')[-1].split('_')[0],
-                key="selectbox"
-            )
-            selected_file = files[choice - 1]
-            break
-        except IndexError:
-            st.sidebar.warning("Invalid choice. Please try again.")
+    selected_files = []
+    for _ in range(2):
+        while True:
+            try:
+                choice = st.sidebar.selectbox(
+                    "Select Company Ticker",
+                    range(1, num_files + 1),
+                    format_func=lambda x: files[x - 1].split('/')[-1].split('_')[0],
+                    key=f"selectbox_{_}"
+                )
+                selected_file = files[choice - 1]
+                selected_files.append(selected_file)
+                break
+            except IndexError:
+                st.sidebar.warning("Invalid choice. Please try again.")
 
-    return selected_file
+    return selected_files
 
 # the path to your csv file directory
 mycsvdir = os.path.expanduser('~/Documents/data')
@@ -62,48 +67,88 @@ mycsvdir = os.path.expanduser('~/Documents/data')
 # get all the csv files in that directory (assuming they have the extension .csv)
 csvfiles = glob.glob(os.path.join(mycsvdir, '*.csv'))
 
-# Prompt the user to select one file
-selected_file = select_file(csvfiles)
+# Prompt the user to select two files
+selected_files = select_files(csvfiles)
 
-# Read the selected file using pandas
-df = pd.read_csv(selected_file)
-df = df[['Date', 'Close']]
-df.columns = ['ds', 'y']
-df['ds'] = pd.to_datetime(df['ds'])
-df.reset_index(drop=True, inplace=True)
+# Read the selected files using pandas
+dfs = []
+for selected_file in selected_files:
+    df = pd.read_csv(selected_file)
+    df = df[['Date', 'Close']]
+    df.columns = ['ds', 'y']
+    df['ds'] = pd.to_datetime(df['ds'])
+    df.reset_index(inplace=True, drop=True)
+    dfs.append(df)
 
-# Plot the selected file
-title = f'Chart of Original Vs Predicted for ({selected_file.split("/")[-1].split("_")[0]})'
+# Plot the selected files
+titles = []
+tickers = []
+for selected_file in selected_files:
+    ticker = selected_file.split('/')[-1].split('_')[0]
+    tickers.append(ticker)
+    selected_file = selected_file.replace(mycsvdir + '/', '')  # Remove the directory path
+    selected_file = selected_file.replace('.csv', '')  # Remove the ".csv" extension
+    #selected_file = selected_file.replace('data"\"', '')  # Remove the ".data" extension
+    ticker = ticker.replace('data"\"', '')  # Remove the ".data" extension
+    #titles.append(f'Original Vs Predicted ({ticker})')
+    titles.append(f'Chart of Original Vs Predicted for ({ticker})')
 
-# Split the data into testing and training datasets
-train = df[df['ds'] <= '10/31/2023']
-test = df[df['ds'] >= '11/01/2023']
+def interactive_plot_forecasting(df, forecast, title):
+    fig = px.line(df, x='ds', y=['y', 'predicted'], title=title)
 
-st.title("Major US Stocks Forecast Wizard")
-st.write("")
-st.subheader("The Smart Stock Trend Wiz: $$$")
-st.write(selected_file.split("/")[-1].split("_")[0])
-st.write("How to read chart: Below yhat_lower --> buy signal, above yhat_upper --> sell signal")
-st.write(f"Number of days in train data: {len(train)}")
-st.write(f"Number of days in test data: {len(test)}")
+    # Get maximum and minimum points
+    max_points = df[df['y'] == df['y'].max()]
+    min_points = df[df['y'] == df['y'].min()]
 
-# Initialize Model
-m = Prophet()
+    # Add maximum points to the plot
+    fig.add_trace(go.Scatter(x=max_points['ds'], y=max_points['y'], mode='markers', name='Maximum'))
 
-# Create and fit the prophet model to the training data
-m.fit(train)
+    # Add minimum points to the plot
+    fig.add_trace(go.Scatter(x=min_points['ds'], y=min_points['y'], mode='markers', name='Minimum'))
 
-# Make predictions
-future = m.make_future_dataframe(periods=93)
-forecast = m.predict(future)
+    # Add yhat_lower and yhat_upper
+    fig.add_trace(go.Scatter(x=df['ds'], y=forecast['yhat_lower'], mode='lines', name='yhat_lower'))
+    fig.add_trace(go.Scatter(x=df['ds'], y=forecast['yhat_upper'], mode='lines', name='yhat_upper'))
 
-# Add predicted values to the original dataframe
-df['predicted'] = forecast['trend']
+    st.plotly_chart(fig)
 
-# Plot the forecast and the original values for comparison
-fig = px.line(df, x='ds', y=['y', 'predicted'], title=title)
-fig.update_layout(xaxis_title="Date", yaxis_title="Price")
-st.plotly_chart(fig)
+# Append today's date to the titles
+today = date.today().strftime("%Y-%m-%d")
 
-# Delete the selected file
-os.remove(selected_file.replace('\\', '/'))
+# Iterate over the selected files and their corresponding titles
+for df, title, ticker in zip(dfs, titles, tickers):
+    # Split the data into testing and training datasets
+    train = df[df['ds'] <= '10/31/2023']
+    test = df[df['ds'] >= '11/01/2023']
+
+    st.title("Major US Stocks Forecast Wizard")
+    st.write("")
+    st.subheader("The Smart Stock Trend Wiz: $$$")
+    st.write({ticker})
+    st.write("How to read chart: Below yhat_lower --> buy signal, above yhat_upper --> sell signal")
+    #st.write(f"Number of months in train data for {ticker}: {len(train)}")
+    #st.write(f"Number of months in test data for {ticker}: {len(test)}")
+    st.write(f"Number of days in train data: {len(train)}")
+    st.write(f"Number of days in test data: {len(test)}")
+
+    # Initialize Model
+    m = Prophet()
+
+    # Create and fit the prophet model to the training data
+    m.fit(train)
+
+    # Make predictions
+    future = m.make_future_dataframe(periods=93)
+    forecast = m.predict(future)
+    #st.write("Forecast for", ticker)
+    #   st.write(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(30))
+
+    # Add predicted values to the original dataframe
+    df['predicted'] = forecast['trend']
+
+    # Plot the forecast and the original values for comparison
+    interactive_plot_forecasting(df, forecast, f'{title} ({today})')
+
+# Delete existing files
+for file in csvfiles:
+    os.remove(file.replace('\\', '/'))
