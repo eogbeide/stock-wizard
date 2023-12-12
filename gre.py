923e49185ac698259b4c83e5aa0e1a8222040100
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-12 07:48:10 -0800

    Create gre.py

---
+++ gre.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+
+# Function to scrape the top 500 GRE words from the website
+def scrape_gre_words():
+    url = "https://www.majortests.com/gre/wordlist.php"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    word_table = soup.find("table", {"class": "wordlist"})
+    rows = word_table.find_all("tr")[1:]  # Skip the table header row
+
+    words = []
+    for row in rows:
+        cells = row.find_all("td")
+        word = cells[1].text.strip()
+        meaning = cells[2].text.strip()
+        example = cells[3].text.strip()
+        words.append({"Word": word, "Meaning": meaning, "Example": example})
+
+    return words
+
+# Streamlit app
+def main():
+    st.title("Top 500 GRE Words")
+    st.write("Choose a word to get its meaning and an example sentence.")
+
+    # Scrape the top 500 GRE words
+    words = scrape_gre_words()
+
+    # Create a dictionary mapping words to meanings and example sentences
+    words_dict = {word["Word"]: {"Meaning": word["Meaning"], "Example": word["Example"]} for word in words}
+
+    # Select a word from the dropdown
+    selected_word = st.selectbox("Select a word", list(words_dict.keys()))
+
+    # Display the meaning and example sentence
+    st.write(f"Meaning: {words_dict[selected_word]['Meaning']}")
+    st.write(f"Example: {words_dict[selected_word]['Example']}")
+
+if __name__ == '__main__':
+    main()
