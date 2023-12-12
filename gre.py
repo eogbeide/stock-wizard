337509143437d8d1020cdbe3b1553ee21dd289cc
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-12 09:36:54 -0800

    Update gre.py

--- gre.py
+++ gre.py
@@ -1,51 +1,26 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
 
-# Function to scrape the top 500 GRE words from the website
-def scrape_gre_words():
-    url = "https://www.majortests.com/gre/wordlist.php"
+# Load the top 500 GRE words and their meanings from a CSV file
+words_df = pd.read_csv('top_500_gre_words.csv')
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    word_table = soup.find("table", {"class": "wordlist"})
-    if word_table is None:
-        return []
-
-    rows = word_table.find_all("tr")[1:]  # Skip the table header row
-
-    words = []
-    for row in rows:
-        cells = row.find_all("td")
-        word = cells[1].text.strip()
-        meaning = cells[2].text.strip()
-        example = cells[3].text.strip()
-        words.append({"Word": word, "Meaning": meaning, "Example": example})
-
-    return words
+# Create a dictionary mapping words to meanings and example sentences
+words_dict = {
+    row['Word']: {'Meaning': row['Meaning'], 'Example': row['Example']}
+    for _, row in words_df.iterrows()
+}
 
 # Streamlit app
 def main():
     st.title("Top 500 GRE Words")
     st.write("Choose a word to get its meaning and an example sentence.")
 
-    # Scrape the top 500 GRE words
-    words = scrape_gre_words()
-
-    # Create a dictionary mapping words to meanings and example sentences
-    words_dict = {word["Word"]: {"Meaning": word["Meaning"], "Example": word["Example"]} for word in words}
-
     # Select a word from the dropdown
     selected_word = st.selectbox("Select a word", list(words_dict.keys()))
 
-    # Check if the selected word exists in the dictionary
-    if selected_word in words_dict:
-        # Display the meaning and example sentence
-        st.write(f"Meaning: {words_dict[selected_word]['Meaning']}")
-        st.write(f"Example: {words_dict[selected_word]['Example']}")
-    else:
-        st.write("Word not found.")
+    # Display the meaning and example sentence
+    st.write(f"Meaning: {words_dict[selected_word]['Meaning']}")
+    st.write(f"Example: {words_dict[selected_word]['Example']}")
 
 if __name__ == '__main__':
     main()
