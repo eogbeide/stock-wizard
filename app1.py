aaa80c19f943d381b977b7a415ddeba769d20f60
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-14 05:57:08 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -169,7 +169,7 @@ for df, title, ticker in zip(dfs, titles, tickers):
 
     # Display today's forecast values
     if yesterday_actual_price is not None:
-    st.write("- Yesterday's Price: ", yesterday_actual_price)
+        st.write("- Yesterday's Price: ", yesterday_actual_price)
     st.write("Today's Forecast Confidence Intervals:")
     st.write("- yhat: ", today_yhat)
     st.write("- yhat_lower: ", today_yhat_lower)
