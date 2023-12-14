4a691e9c5b45194cd5df11cb4a10f025e1c9dae9
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-14 15:13:11 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -191,7 +191,11 @@ for file in csvfiles:
     os.remove(file.replace('\\', '/'))
 
 # Display selected ticker information
-st.write("Selected Ticker Information:")
-st.write(selected_ticker_info)
-st.write("Company Name: ", selected_ticker_info['longName'])
+#st.write("Selected Ticker Information:")
+#st.write(selected_ticker_info)
+st.subheader("Company Name: ", selected_ticker_info['longName'])
 st.write("Location: ", selected_ticker_info['country'])
+st.write("Location: ", selected_ticker_info['fiftyDayAverage'])
+st.write("Location: ", selected_ticker_info['twoHundredDayAverage'])
+st.write("Location: ", selected_ticker_info['beta'])
+st.write("Company Name: ", selected_ticker_info['longBusinessSummary'])
