436fee528b2d0427f4f0ef207daeeb5b4093b116
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-14 05:23:08 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -158,7 +158,7 @@ for df, title, ticker in zip(dfs, titles, tickers):
     today_yhat_upper = round(today_forecast['yhat_upper'].values[0],2)
 
     # Display today's forecast values
-    st.write("Today's Forecast:")
+    st.write("Today's Forecast Confidence Intervals:")
     st.write(f"yhat: {today_yhat}")
     st.write(f"yhat_lower: {today_yhat_lower}")
     st.write(f"yhat_upper: {today_yhat_upper}")
