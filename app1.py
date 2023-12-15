65eb78931d6833dd2f198da54f853e2147e74cba
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-15 07:39:03 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -192,16 +192,11 @@ for file in csvfiles:
     os.remove(file.replace('\\', '/'))
 
 # Display selected ticker information
-st.write("Selected Ticker Information:")
-# st.write(selected_ticker_info)
+#st.write("Selected Ticker Information:")
+#st.write(selected_ticker_info)
 st.write("Other Stats")
 st.write(" - 50-Day Average: ", selected_ticker_info['fiftyDayAverage'])
 st.write(" - 200-Day Average: ", selected_ticker_info['twoHundredDayAverage'])
 st.write(" - beta: ", selected_ticker_info['beta'])
 st.subheader("About Company")
-#if 'longBusinessSummary' in selected_ticker_info:
-    #st.write(selected_ticker_info['longBusinessSummary'])
-if 'beta != 0' in selected_ticker_info:
-    st.write(selected_ticker_info['beta'])
-else:
-    st.write("Not Available")
+st.write(selected_ticker_info['longBusinessSummary'])
