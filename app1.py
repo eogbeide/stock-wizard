c9c3c86fa00fb0730d1ee93a2a5001bdb1aa694e
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-16 06:28:28 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -135,7 +135,7 @@ for df, title, ticker in zip(dfs, titles, tickers):
     st.title("Major US Stocks AI Forecast Wizard")
     st.write("")
     st.subheader("The Smart AI Stock Trend Wiz by Manny: $$$")
-    st.write("Company Name: ", selected_ticker_info['longName'])
+    st.write(" - Company Name: ", selected_ticker_info['longName'])
     #st.write(" - Location: ", selected_ticker_info['country'])
     st.subheader("How to read chart:")
     st.write(" - yhat is the median price that shows price trend")
