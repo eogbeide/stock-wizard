1558190b1ac0abb15fc7c7f5141498db8b90d257
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-16 08:57:15 -0800

    Update satwords.py

--- satwords.py
+++ satwords.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import PyPDF2
 import re
+import urllib
 
 # Streamlit app
 def main():
-    st.title("Top SAT Words by Manny")
+    st.title("SAT Vocabulary Viewer")
     st.write("Choose a word to get its meaning.")
 
     try:
         # Get the raw URL of the PDF file on GitHub
-        raw_pdf_url = "https://raw.githubusercontent.com/eogbeide/stock-wizard/main/SAT_Vocabulary_List.pdf"
+        raw_pdf_url = "https://github.com/eogbeide/stock-wizard/raw/main/SAT_Vocabulary_List.pdf"
 
         # Download the PDF file and read its contents
         pdf_content = st.binary_resource(raw_pdf_url)
@@ -18,32 +19,14 @@ def main():
         # Extract words and meanings from the PDF
         words_dict = extract_words_and_meanings(pdf_file)
 
-        # Group words by alphabet bins
-        alphabet_bins = {}
-        for word in words_dict.keys():
-            letter = word[0].upper()
-            if letter in alphabet_bins:
-                alphabet_bins[letter].append(word)
-            else:
-                alphabet_bins[letter] = [word]
+        # Select a word from the dropdown
+        selected_word = st.selectbox("Select a word", list(words_dict.keys()))
 
-        # Select an alphabet bin from the dropdown
-        selected_bin = st.selectbox("Select an alphabet bin", sorted(alphabet_bins.keys()))
-
-        # Filter the words based on the selected alphabet bin
-        filtered_words = {word: words_dict[word] for word in alphabet_bins[selected_bin]}
-
-        if filtered_words:
-            # Select a word from the filtered words
-            selected_word = st.selectbox("Select a word", list(filtered_words.keys()))
-
-            if selected_word:
-                # Display the meaning of the selected word
-                st.write(f"Meaning: {filtered_words[selected_word]}")
-            else:
-                st.write("Please select a word.")
+        if selected_word:
+            # Display the meaning of the selected word
+            st.write(f"Meaning: {words_dict[selected_word]}")
         else:
-            st.write("No words found for the selected alphabet bin.")
+            st.write("Please select a word.")
 
     except PyPDF2.PdfReadError:
         st.error("Error: Unable to read the PDF file.")
