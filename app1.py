3bd5e995c20bd6b55ef9cb1123e16474c027ec7b
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-17 16:55:45 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -100,7 +100,7 @@ def select_files(files):
     return selected_files, selected_ticker_info
 
 companies = st.multiselect(
-    "Filter the company:", options=Ticker.sort_values(by="Ticker").Ticker.unique()
+    "Filter the company:", options=ticker_list.sort_values(by="Ticker").ticker_list.unique()
 )
 
 # the path to your csv file directory
