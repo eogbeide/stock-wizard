7e88a38f5c4b1cf8d9408fc1cd57ed74812cb4b5
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-17 17:34:06 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,10 +22,10 @@ ticker_list = sorted (['LLY','V','MA','ABBV','WBA','BMY','HUM','CI','UNH','CVS',
 #ticker_list_sorted = sorted(ticker_list)
 #ticker_list = ticker_list_sorted
 
-def load_data():
-    tickers = ticker_list
-    return pd.DataFrame({"Ticker": tickers})
-df = load_data()
+#def load_data():
+    #tickers = ticker_list
+   # return pd.DataFrame({"Ticker": tickers})
+#df = load_data()
 
 today = date.today()
 # Check if today is a weekend (Saturday or Sunday)
