6878b5c8f3b883ed3facb0f96986377f355944b8
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-17 17:41:48 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -220,15 +220,15 @@ for df, title, ticker in zip(dfs, titles, tickers):
     # Check if yesterday's date falls on a weekend
     if yesterday.weekday() >= 5:
         # Display message for weekend
-        error_message = "Yesterday's actual price is unavailable on weekends and Mondays"
+        error_message = "Yesterday's actual close price is unavailable on weekends and Mondays"
         print(error_message)
-        st.write("- Yesterday's actual price is unavailable on weekends and Mondays")
+        st.write("- Yesterday's actual close price is unavailable on weekends and Mondays")
     else:
         # Continue with the rest of your code
         print("Yesterday's actual price is available")
         # Get yesterday's actual price
         yesterday_actual_price = round(df[df['ds'] == yesterday]['y'].values[0],2)
-        st.write("- Yesterday's Price: ", yesterday_actual_price)
+        st.write("- Yesterday's Closing Price: ", yesterday_actual_price)
         
 
     # Check if yesterday's actual price exists
