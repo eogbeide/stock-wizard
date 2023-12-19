b2a0aa1cae50ce6fb348dac54f57943ecb678dfa
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-19 03:15:34 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -223,23 +223,22 @@ for df, title, ticker in zip(dfs, titles, tickers):
     yesterday = today - datetime.timedelta(days=1)
 
     # Check if yesterday's date falls on a weekend
-    #if yesterday.weekday() >= 5:
-        # Display message for weekend
-        #error_message = "Yesterday's actual close price is unavailable on weekends and Mondays"
-       # print(error_message)
-        #st.write("- Yesterday's actual close price is unavailable on weekends and Mondays")
-    #else:
-        # Continue with the rest of your code
-        #print("Yesterday's actual price is available")
-        # Get yesterday's actual price
-        #yesterday_actual_price = round(df[df['ds'] == yesterday]['y'].values[0],2)
-        #st.write("- Yesterday's Closing Price: ", yesterday_actual_price)
+    if yesterday.weekday() >= 5:
+        Display message for weekend
+        error_message = "Yesterday's actual close price is unavailable on weekends and Mondays"
+        print(error_message)
+        st.write("- Yesterday's actual close price is unavailable on weekends and Mondays")
+    else:
+        Continue with the rest of your code
+        print("Yesterday's actual price is available")
+        #Get yesterday's actual price
+        yesterday_actual_price = round(df[df['ds'] == yesterday]['y'].values[0],2)
+        st.write("- Yesterday's Closing Price: ", yesterday_actual_price)
         
 
     # Check if yesterday's actual price exists
     st.subheader("Yesterday's Closing Price:")
     # Get yesterday's actual price
-    yesterday_actual_price = round(df[df['ds'] == yesterday]['y'].values[0],2)
     if yesterday in df['ds'].values:
         yesterday_actual_price = df[df['ds'] == yesterday]['y'].values[0]
 
