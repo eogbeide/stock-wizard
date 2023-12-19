db1844d42fb83ab7fc03de74deea0a308fb80973
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-19 07:45:38 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -200,6 +200,7 @@ future = m.make_future_dataframe(periods=93)
 forecast = m.predict(future)
 #st.write("Forecast for", ticker)
 #st.write(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(30))
+st.write((df).tail(5))
 
 # Add predicted values to the original dataframe
 df['predicted'] = forecast['trend']
