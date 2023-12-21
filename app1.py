4efe6a562ae2db36536df60a4f6b93b96633a571
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-21 00:31:51 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -174,21 +174,6 @@ for df, title, ticker in zip(dfs, titles, tickers):
     train = df[df['ds'] <= '10/31/2023']
     test = df[df['ds'] >= '11/01/2023']
 
-    st.title("Major US Stocks AI Forecast Wizard")
-    st.write("")
-    #st.write("The Smart AI Stock Trend Wiz by Manny: $$$")
-    st.write(f" - **Company Name:** ", selected_ticker_info['longName'])
-    #st.write(" - Location: ", selected_ticker_info['country'])
-    st.header("How to read chart:")
-    st.write(f" - **yhat** is the median price that shows price trend")
-    st.write(f" - **yhat_lower** is the lowest price. Actual price below yhat_lower signals a buying opportunity. Below yhat_lower --> Buy Signal")
-    st.write(f" - **yhat_upper** is the highest price. Actual price above yhat_upper signals a selling or profit taking opportunity. Above yhat_upper --> Sell or Profit Taking Signal")
-    #st.write(f"Number of months in train data for {ticker}: {len(train)}")
-    #st.write(f" - Number of months in test data for {ticker}: {len(test)}")
-    st.subheader(f"Machine Learning Modeling Information")
-    st.write(f" - Number of days in training data: {len(train)}")
-    st.write(f" - Number of days in testing data: {len(test)}")
-
 # Initialize Model
 m = Prophet()
 
@@ -211,6 +196,21 @@ interactive_plot_forecasting(df, forecast, f'{title} ({today})')
 st.subheader("Last Three Days Closing Prices")
 df['ds'] = pd.to_datetime(df['ds']).dt.date
 st.write(df[['ds', 'y']].tail(3).reset_index(drop=True))
+
+st.title("Major US Stocks AI Forecast Wizard")
+st.write("")
+#st.write("The Smart AI Stock Trend Wiz by Manny: $$$")
+st.write(f" - **Company Name:** ", selected_ticker_info['longName'])
+#st.write(" - Location: ", selected_ticker_info['country'])
+st.header("How to read chart:")
+st.write(f" - **yhat** is the median price that shows price trend")
+st.write(f" - **yhat_lower** is the lowest price. Actual price below yhat_lower signals a buying opportunity. Below yhat_lower --> Buy Signal")
+st.write(f" - **yhat_upper** is the highest price. Actual price above yhat_upper signals a selling or profit taking opportunity. Above yhat_upper --> Sell or Profit Taking Signal")
+#st.write(f"Number of months in train data for {ticker}: {len(train)}")
+#st.write(f" - Number of months in test data for {ticker}: {len(test)}")
+st.subheader(f"Machine Learning Modeling Information")
+st.write(f" - Number of days in training data: {len(train)}")
+st.write(f" - Number of days in testing data: {len(test)}")
 
 # Extract today's forecast values
 today_forecast = forecast[forecast['ds'] == today]
