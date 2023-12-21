936c1c6c49b939211c310a4528b0891821a3562f
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-21 02:04:24 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -160,7 +160,7 @@ def add_moving_average(df, window=7):
 def interactive_plot_forecasting(df, forecast, title):
     # Add moving average to the DataFrame
     df = add_moving_average(df, window=7)
-    fig = px.line(df, x='ds', y=['y', 'predicted', 'moving_avg], title=title)
+    fig = px.line(df, x='ds', y=['y', 'predicted', 'moving_avg'], title=title)
 
     # Get maximum and minimum points
     max_points = df[df['y'] == df['y'].max()]
