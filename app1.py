aec47c5ab39aaa6210bac5e131346e7d14f7ee18
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-21 01:09:22 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -204,7 +204,7 @@ interactive_plot_forecasting(df, forecast, f'{title} ({today})')
 st.subheader("Last Three Days Closing Prices")
 df['ds'] = pd.to_datetime(df['ds']).dt.date
 #st.write(df[['ds', 'y']].tail(3).reset_index(drop=True))
-st.write(df.tail(3).set_index(df.columns[0]))
+st.write(df[['ds', 'y']].tail(3).set_index(df.columns[0]))
 
 #st.write(" - Location: ", selected_ticker_info['country'])
 st.header("How to read chart:")
