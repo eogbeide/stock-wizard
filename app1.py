3f140ed28e3e7cf607b8b69743f5b096478475ab
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-28 09:17:01 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -92,7 +92,7 @@ def select_files(files):
             # Display the sorted ticker list in Streamlit
             choice = st.sidebar.selectbox(
                 "Select Company Ticker",
-                options=df.sort_values(by="Ticker").tickers.unique()
+                options=df.sort_values(by="Ticker").tickers.unique(),
                 range(1, num_files + 1), 
                 format_func=lambda x: files[x - 1].split('/')[-1].split('_')[0],
                 key="selectbox"
