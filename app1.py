ec75722c4bdbaa8528b956dc1b003bf855bcffe6
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-02 13:42:11 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -272,10 +272,10 @@ else:
 
 
 # Resample the data to weekly frequency and calculate the mean price for each week
-weekly_df = df.resample('W', on='date').mean()
+weekly_df = df.resample('W', on='ds').mean()
 
 # Create an interactive line plot using plotly express
-fig = px.line(weekly_df, x='date', y='price')
+fig = px.line(weekly_df, x='ds', y='y')
 
 # Add title and axis labels
 fig.update_layout(
