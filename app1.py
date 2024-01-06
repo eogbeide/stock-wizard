c56a992dc73ef9844cb9d6debd7abbd8a906a71d
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-06 05:26:09 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -241,9 +241,9 @@ data = {
 }
 
 # Display the DataFrame as a three-column table
-#st.subheader("Current Forecast Price Confidence Intervals:")
-#st.write(df)
-#st.write(df.set_index(df.columns[0]))
+st.subheader("Current Forecast Price Confidence Intervals:")
+st.write(df)
+st.write(df.set_index(df.columns[0]))
 
 #st.write("Forecast for", ticker)
 #forecast['ds'] = forecast['ds'].dt.date
