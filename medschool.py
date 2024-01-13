2eb0ff30ead60962a3dae55008abb96fba7d74fe
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-12 19:04:59 -0800

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -10,8 +10,14 @@ school_options = df['Medical School'].unique().tolist()
 # Create a selectbox to choose a medical school
 selected_school = st.sidebar.selectbox("Select a Medical School", school_options)
 
-# Filter the DataFrame based on the selected school and exclude State and Medical School columns
-filtered_df = df[df['Medical School'] == selected_school].drop(columns=['State', 'Medical School'])
+# Create a list of distinct state names
+state_options = df['State'].unique().tolist()
 
-# Display the filtered DataFrame without the index column
-st.dataframe(filtered_df.reset_index(drop=True))
+# Create a selectbox to choose a state
+selected_state = st.sidebar.selectbox("Select a State", state_options)
+
+# Filter the DataFrame based on the selected school and state, and exclude State and Medical School columns
+filtered_df = df[(df['Medical School'] == selected_school) & (df['State'] == selected_state)].drop(columns=['State', 'Medical School'])
+
+# Display the filtered DataFrame without the index column, with text wrapping
+st.dataframe(filtered_df.reset_index(drop=True).style.set_properties(**{'white-space': 'pre-wrap'}))
