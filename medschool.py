7c2633dac3e514a46c98402494772349ae0828aa
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-12 19:02:41 -0800

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-# Load the Excel file
+# Load the CSV file
 df = pd.read_csv("Medical_School_Requirements.csv")
 
-# Create a list of medical school options
-school_options = df['Medical School'].tolist()
+# Create a list of distinct medical school names
+school_options = df['Medical School'].unique().tolist()
 
 # Create a selectbox to choose a medical school
 selected_school = st.sidebar.selectbox("Select a Medical School", school_options)
 
-# Filter the DataFrame based on the selected school
-filtered_df = df[df['Medical School'] == selected_school]
+# Filter the DataFrame based on the selected school and exclude State and Medical School columns
+filtered_df = df[df['Medical School'] == selected_school].drop(columns=['State', 'Medical School'])
 
 # Display the filtered DataFrame
 st.dataframe(filtered_df)
