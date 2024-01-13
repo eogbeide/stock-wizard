8b6f63231f0b74d63ecb434314c5b18cb73f6de3
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-12 20:02:53 -0800

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -19,8 +19,12 @@ selected_school = st.sidebar.selectbox("Select a Medical School", state_filtered
 # Filter the DataFrame based on the selected school and exclude State and Medical School columns
 filtered_df = state_filtered_df[state_filtered_df['Medical School'] == selected_school].drop(columns=['State', 'Medical School'])
 
-# Format the 'Credit Hours' column to display one significant figure
-filtered_df['Credit Hours'] = filtered_df['Credit Hours'].apply(lambda x: format(x, ".1g"))
+# Check if 'Credit Hours' column exists in the filtered DataFrame
+if 'Credit Hours' in filtered_df.columns:
+    # Format the 'Credit Hours' column to display one significant figure
+    filtered_df['Credit Hours'] = filtered_df['Credit Hours'].apply(lambda x: format(x, ".1g"))
 
-# Display the filtered DataFrame without the index column, with wrapped text in the 'Additional Info' column
-st.dataframe(filtered_df.reset_index(drop=True).style.set_properties(**{'white-space': 'pre-wrap'}))
+    # Display the filtered DataFrame without the index column, with wrapped text in the 'Additional Info' column
+    st.dataframe(filtered_df.reset_index(drop=True).style.set_properties(**{'white-space': 'pre-wrap'}))
+else:
+    st.write("No 'Credit Hours' column found in the filtered DataFrame.")
