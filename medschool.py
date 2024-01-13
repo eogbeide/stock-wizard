aebc67655feafa7b1d715fd4122d23b89168ec36
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-13 06:09:56 -0800

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -7,14 +7,18 @@ df = pd.read_csv("Medical_School_Requirements2.csv")
 # Remove leading and trailing spaces from all string columns
 df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
+# Create a selectbox to choose a state
+selected_state = st.sidebar.selectbox("Select a State", df['State'].unique())
+
+# Filter the DataFrame based on the selected state
+state_filtered_df = df[df['State'] == selected_state]
+
 # Create a list of distinct medical school names
-school_options = df['Medical School'].unique().tolist()
+school_options = state_filtered_df['Medical School'].unique().tolist()
 
 # Page 1: Filter by state and medical school
 st.sidebar.title("Page 1: Filter")
-selected_state = st.sidebar.selectbox("Select a State", df['State'].unique())
-state_filtered_df = df[df['State'] == selected_state]
-selected_school = st.sidebar.selectbox("Select a Medical School", state_filtered_df['Medical School'].unique())
+selected_school = st.sidebar.selectbox("Select a Medical School", school_options)
 filtered_df = state_filtered_df[state_filtered_df['Medical School'] == selected_school].drop(columns=['State', 'Medical School'])
 
 # Check if 'Credit Hours' column exists in the filtered DataFrame
