55831f1808a719b03c08643aefd333005fc4f313
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-14 15:07:31 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -227,7 +227,7 @@ interactive_plot_forecasting(df, forecast, f'{title} ({today})')
 st.subheader("Last Three Days Closing Prices")
 df['ds'] = pd.to_datetime(df['ds']).dt.date
 #st.write(df[['ds', 'y']].tail(3).reset_index(drop=True))
-st.write(df[['ds', 'y']].tail(3).set_index(df.columns[0]))
+#st.write(df[['ds', 'y']].tail(3).set_index(df.columns[0]))
 
 # Extract today's forecast values
 today_forecast = forecast[forecast['ds'] == today]
