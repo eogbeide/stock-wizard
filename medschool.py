4cd414168e9393751927635af51ba91ced14be75
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-14 05:53:50 -0800

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -48,6 +48,7 @@ st.link_button(f"Go to AAMC website for {search_query}", "https://members.aamc.o
 
 # Filter the DataFrame based on the selected school and exclude State and Medical School columns
 filtered_df = state_filtered_df[state_filtered_df['Medical School'] == selected_school].drop(columns=['State', 'Medical School'])
+st.markdown(filtered_df.to_html(escape=False), unsafe_allow_html=True)
 
 # Check if 'Credit Hours' column exists in the filtered DataFrame
 if 'Credit Hours' in filtered_df.columns:
@@ -82,4 +83,3 @@ columns_to_display = ['Medical School', 'Lab?', 'Credit Hours', 'Additional Info
 # Display the filtered DataFrame with the selected columns, sorted by "Medical School"
 st.dataframe(filtered_schools_df[columns_to_display].sort_values(by="Medical School").reset_index(drop=True))
 
-st.markdown(filtered_df.to_html(escape=False), unsafe_allow_html=True)
