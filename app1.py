defa165718ef6cab64a868235416460c8a681e6e
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-01-25 14:29:06 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -222,7 +222,7 @@ st.write(f" - Number of days in training data: {len(train)}")
 st.write(f" - Number of days in testing data: {len(test)}")
 
 # Select the last 24 months of data
-df_last_24_months = df.tail(720)
+df_last_24_months = df.tail(1080)
 forecast_last_24_months = forecast.tail(720)
 
 # Plot the forecast and the original values for comparison
