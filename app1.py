3df347ab639ba14b40575f495a280f613288da45
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-02-09 12:27:12 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -272,9 +272,18 @@ st.write(df[['ds', 'y']].tail(3).set_index(df.columns[0]))
 today_forecast = forecast[forecast['ds'] == today]
 
 # Get today's yhat, yhat_lower, and yhat_upper values
-today_yhat = round(today_forecast['yhat'].values[0],2)
-today_yhat_lower = round(today_forecast['yhat_lower'].values[0],2)
-today_yhat_upper = round(today_forecast['yhat_upper'].values[0],2)
+#today_yhat = round(today_forecast['yhat'].values[0],2)
+#today_yhat_lower = round(today_forecast['yhat_lower'].values[0],2)
+#today_yhat_upper = round(today_forecast['yhat_upper'].values[0],2)
+
+if len(today_forecast) > 0:
+    today_yhat = round(today_forecast['yhat'].values[0], 2)
+    today_yhat_lower = round(today_forecast['yhat_lower'].values[0], 2)
+    today_yhat_upper = round(today_forecast['yhat_upper'].values[0], 2)
+else:
+    today_yhat = None  # or assign a default value
+    today_yhat_lower = None  # or assign a default value
+    today_yhat_upper = None  # or assign a default value
 
 # Get today's date as a datetime.date object
 today = datetime.date.today()
