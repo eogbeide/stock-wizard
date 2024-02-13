30919f79bb522040aefd04e4f31916215163540e
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-02-13 13:27:18 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -51,7 +51,7 @@ else:
     st.write("Welcome to the Smart AI Stock Trend Prediction Wizard by Manny: $$$")
     
 # We can get data by our choice by giving days bracket
-start_date = "2015-10-01"
+start_date = "2022-10-01"
 end_date = today.strftime("%Y-%m-%d")  # Use today's date as the end date
 
 # Get yesterday's date
