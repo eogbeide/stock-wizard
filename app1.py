d1109f7b8ff627e06fdca0b5eb5970601fee79f4
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-03-01 09:48:04 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -333,7 +333,7 @@ st.write("Forecast for", ticker)
 forecast['ds'] = forecast['ds'].dt.date
 forecast.reset_index(drop=True)
 forecast.reset_index(drop=True, inplace=True)
-st.write(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(10)).set_index(forecast.columns[0])
+st.write(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(10))
 #st.write(forecast[['ds', 'yhat_lower', 'yhat_upper']].tail(30)).set_index(forecast.columns[0])
 #forecast = forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(10).set_index(forecast.columns[0])
 
