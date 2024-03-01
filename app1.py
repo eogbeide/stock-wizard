5161ad10fd9529e870200738813b2ba9eee1226a
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-03-01 09:26:42 -0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -335,7 +335,7 @@ forecast.reset_index(drop=True)
 forecast.reset_index(drop=True, inplace=True)
 st.write(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(30))
 #st.write(forecast[['ds', 'yhat_lower', 'yhat_upper']].tail(30))
-forecast = forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(10).set_index(forecast.columns[0])
+#forecast = forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(10).set_index(forecast.columns[0])
 
 #st.write(forecast)
 
