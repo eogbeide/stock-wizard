8cd87a152f247e92d798007dd42bb454cfa7e88a
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-07-11 18:54:41 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -382,4 +382,3 @@ else:
 
 st.title("DISCLAIMER")
 st.write("Please note that the information provided in this app does not replace professional advice from licensed finance professionals and brokers. Due to the inherent risks in stock trading, it is advised that users consult with professionals before making any financial decisions.")
-d
