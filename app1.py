786fc28979155a578099d13cf17d7c9d571521f7
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-07-26 03:05:32 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -162,7 +162,7 @@ def calculate_trend_break(df):
     #df['trend'] = np.float64.where(np.greater(df['y'].diff(), 50), 'up', 'down')
     df['trend_break'] = np.float64(np.not_equal(df['trend'].shift(), df['trend']))
     #df['trend_break'] = np.float64.where(df['trend'].shift() != df['trend'], 1, 0)
-    df['trend_break'] = np.float.64.where(np.not_equal(df['trend'].shift(), df['trend']), 1, 0)
+    #df['trend_break'] = np.float.64.where(np.not_equal(df['trend'].shift(), df['trend']), 1, 0)
     return df
 
 #@st.cache_data(experimental_allow_widgets=True)
