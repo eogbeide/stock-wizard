a6f4832c23cbde3981c62fddbfe824d80ea5362e
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-08-28 11:10:43 -0700

    Update mcat.py

--- mcat.py
+++ mcat.py
@@ -1,5 +1,7 @@
+import requests
 import docx
 import streamlit as st
+import os
 
 class Question:
     def __init__(self, text, choices, answer, explanation):
@@ -10,6 +12,17 @@ class Question:
 
     def check_answer(self, user_answer):
         return user_answer == self.answer
+
+
+def download_docx(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open("mcat.docx", "wb") as f:
+            f.write(response.content)
+        return "mcat.docx"
+    else:
+        st.error("Failed to download the document.")
+        return None
 
 
 def read_questions_from_docx(file_path):
@@ -50,9 +63,13 @@ def take_quiz(questions):
 
 
 if __name__ == "__main__":
-    # Use raw string notation for the file path
-    file_path = r"C:\Users\eogbeide\Documents\mcat.docx"  # Update with your actual username
-    quiz_questions = read_questions_from_docx(file_path)
-
-    st.title("Multiple Choice Quiz")
-    take_quiz(quiz_questions)
+    raw_docx_url = "https://raw.githubusercontent.com/eogbeide/stock-wizard/main/mcat.docx"
+    
+    # Download the docx file
+    file_path = download_docx(raw_docx_url)
+    
+    if file_path:
+        quiz_questions = read_questions_from_docx(file_path)
+        
+        st.title("Multiple Choice Quiz")
+        take_quiz(quiz_questions)
