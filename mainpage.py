0e84c0255dc9d021725015d06dd4c04f989e0da0
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-08-29 23:59:35 -0700

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.title("MCAT Resources")
+st.title("MCAT Resources Designed Just For You")
 
 # Create buttons to navigate to the external apps
 if st.button("Go to MCAT Prep"):
-    st.write("You need to enable JavaScript to run this app.")
+    #st.write("You need to enable JavaScript to run this app.")
     st.markdown("[Open MCAT Prep](https://mcatprep.streamlit.app/)", unsafe_allow_html=True)
 
 if st.button("Go to MCAT Topics"):
-    st.write("You need to enable JavaScript to run this app.")
+    #st.write("You need to enable JavaScript to run this app.")
     st.markdown("[Open MCAT Topics](https://mcattopics.streamlit.app/)", unsafe_allow_html=True)
