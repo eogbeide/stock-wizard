453a3f3e7eb308ad88536c33090f9cfb4efd67b6
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-09-02 06:45:51 -0700

    Update flashcards.py

--- flashcards.py
+++ flashcards.py
@@ -26,7 +26,6 @@ def main():
 
     # Clean column names
     df.columns = df.columns.str.strip()  # Remove any leading/trailing whitespace
-    st.write(df.columns)  # Debugging: print column names to check available columns
 
     # Sidebar for subject selection
     subjects = df['Subject'].unique()
