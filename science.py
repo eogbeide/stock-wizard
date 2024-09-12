21d4d80ccf039974b5e950a5c9139e722f97c6cc
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-09-12 12:24:36 -0700

    Update science.py

--- science.py
+++ science.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Create a timestamp to force a refresh
-today = datetime.datetime.now().date()
-st.write(f"Last updated: {today}")
+#today = datetime.datetime.now().date()
+#st.write(f"Last updated: {today}")
 
 # Load data from the CSV file on GitHub with explicit encoding
 url = 'https://raw.githubusercontent.com/eogbeide/stock-wizard/main/science.csv'
