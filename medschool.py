f87deed169fd87ebc5d8016a940631b817457abb
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-09-21 14:09:45 -0700

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Create a timestamp to force a refresh
-today = datetime.datetime.now().date()
-st.write(f"Last updated: {today}")
+#today = datetime.datetime.now().date()
+#st.write(f"Last updated: {today}")
 
 st.set_page_config(layout="wide")
 st.markdown("""
