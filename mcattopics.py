079815bf19b9f868ea7dc96a34517ffb7194b0dc
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-10-08 03:25:39 -0700

    Update mcattopics.py

--- mcattopics.py
+++ mcattopics.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from urllib.error import URLError
 
-<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js"></script>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
