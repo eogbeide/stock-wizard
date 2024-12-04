5bc6d5cecbfbc9a668e44cbd06218fbd5f99fbf9
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-12-04 06:06:03 -0800

    Update medschool.py

--- medschool.py
+++ medschool.py
@@ -30,8 +30,8 @@ st.write(" - Always cross-validate with the school's website")
 st.header("Selected Medical School Prerequisites")
 st.write(" - Y = Yes, N = No and C = Case-by-Case Basis")
 
-# Load the CSV file
 @st.cache_data
+# Load the CSV file
 df = pd.read_csv("Medical_School_Requirements5.csv", encoding='unicode_escape')
 
 # Remove leading and trailing spaces from all string columns
