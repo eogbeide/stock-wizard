8d8fcab9101c8cb940572a63ee4e4466d3088721
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-12-05 03:16:42 -0800

    Update mcat.py

--- mcat.py
+++ mcat.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pandas as pdf
+import pandas as pd
 
-@st.cache_data      
+   
 # Create a timestamp to force a refresh
 #today = datetime.datetime.now().date()
 #st.write(f"Last updated: {today}")
