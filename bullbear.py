35af2ff08bfe98eb407b35cb402264bda0482c4c
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-12-15 08:10:02 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -51,6 +51,27 @@ if st.button("Forecast"):
     plt.plot(ema_200[-180:], label='200-Day EMA', color='green', linestyle='--')  # 200-day EMA
     plt.plot(forecast_index, forecast_values, label='3 Months Forecast', color='orange')
     plt.fill_between(forecast_index, conf_int.iloc[:, 0], conf_int.iloc[:, 1], color='orange', alpha=0.3)
+
+    # Adding the last value annotations
+    last_forecast_value = forecast_values.iloc[-1]
+    last_ema_20_value = ema_20.iloc[-1]
+    last_ema_200_value = ema_200.iloc[-1]
+    
+    plt.annotate(f'Forecast: {last_forecast_value:.2f}', 
+                 xy=(forecast_index[-1], last_forecast_value), 
+                 xytext=(forecast_index[-1], last_forecast_value + 5),
+                 arrowprops=dict(facecolor='black', arrowstyle='->'))
+
+    plt.annotate(f'20-Day EMA: {last_ema_20_value:.2f}', 
+                 xy=(data.index[-1], last_ema_20_value), 
+                 xytext=(data.index[-1], last_ema_20_value + 5),
+                 arrowprops=dict(facecolor='red', arrowstyle='->'))
+
+    plt.annotate(f'200-Day EMA: {last_ema_200_value:.2f}', 
+                 xy=(data.index[-1], last_ema_200_value), 
+                 xytext=(data.index[-1], last_ema_200_value + 5),
+                 arrowprops=dict(facecolor='green', arrowstyle='->'))
+
     plt.title(f'{ticker} Price Forecast for Next 3 Months')
     plt.xlabel('Date')
     plt.ylabel('Price')
