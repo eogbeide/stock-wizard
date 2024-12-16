08d018722db19a0983a156b3ffba26cdb3047e51
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-12-16 12:38:47 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 st.title("Stock Price Forecasting with SARIMA, EMA, and MACD")
 
 # User input for stock ticker using a dropdown menu
-ticker = st.selectbox("Select Stock Ticker:", options=['AAPL', 'SPY', 'AMZN', 'TSLA', 'PLTR'])
+ticker = st.selectbox("Select Stock Ticker:", options=['AAPL', 'SPY', 'AMZN', 'TSLA', 'PLTR', 'NVDA'])
 
 # Button to fetch and process data
 if st.button("Forecast"):
