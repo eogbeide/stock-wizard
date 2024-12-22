9d3b5d3828da34e7a74a72ffc6ef4f5fc662da51
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-12-22 04:39:15 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -35,9 +35,9 @@ st.info(
 
 # User input for stock ticker using a dropdown menu
 ticker = st.selectbox("Select Stock Ticker:", options=[
-    'AAPL', 'SPY', 'AMZN', 'NVO', 'XMTR', 'AMD', 'DIA', 'TSLA','SPGI','SQM','PSTG','QUBT','JNJ','MSTR','ENB','JPM','AFRM','VTWG',
-    'PLTR', 'NVDA', 'META', 'SITM', 'MARA', 'GOOG', 'HOOD', 'BABA','DAR','SFM','AVO','SOUN','MRK','LMND','MO','ISRG','GUSH', 'VOO',
-    'UBER', 'DOW', 'AFRM', 'MSFT', 'TSM', 'NFLX', 'LCID', 'IONQ','SIRI', 'PGR', 'COST', 'RGTI', 'DDS','EQIX', 'FTNT','URI', 'LABU'
+    'AAPL', 'SPY', 'AMZN', 'NVO', 'XMTR', 'AMD', 'DIA', 'TSLA','SPGI','SQM','PSTG','QUBT','JNJ','MSTR','ENB','JPM','AFRM','VTWG','BLOCK','BN',
+    'PLTR', 'NVDA', 'META', 'SITM', 'MARA', 'GOOG', 'HOOD', 'BABA','DAR','SFM','AVO','SOUN','MRK','LMND','MO','ISRG','GUSH', 'VOO','CRM','EPD',
+    'UBER', 'DOW', 'AFRM', 'MSFT', 'TSM', 'NFLX', 'LCID', 'IONQ','SIRI', 'PGR', 'COST', 'RGTI', 'DDS','EQIX', 'FTNT','URI', 'ETSY', 'CNQ', 'CVS'
 ])
 
 # Button to fetch and process data
