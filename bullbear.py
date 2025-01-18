87ca640b85602a29ab2d68cf6f337690382ff032
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-01-17 16:59:08 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -37,8 +37,8 @@ st.info(
 # User input for stock ticker using a dropdown menu
 ticker = st.selectbox("Select Stock Ticker:", options=[
     'AAPL', 'SPY', 'AMZN', 'TSLA','SOFI','TSM',  
-    'NVDA', 'META', 'SITM', 'SPGI','JYD','IONQ', 
-    'MARA', 'GOOG', 'HOOD', 'BABA', 'SMR','SMCI', 
+    'NVDA', 'META', 'SITM', 'SPGI','JYD','ROKU', 
+    'MARA', 'GOOG', 'HOOD', 'BABA', 'SMR', 
     'MSFT', 'DIA', 'NFLX',  'URI', 'VOO'
     ])
 
