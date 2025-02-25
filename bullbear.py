83ec55bab3f1f8a36e76900a9dee824483482bfb
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-02-25 03:53:00 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -36,11 +36,7 @@ st.info(
 
 # User input for stock ticker using a dropdown menu
 ticker = st.selectbox("Select Stock Ticker:", options=[
-    'AAPL', 'SPY', 'AMZN', 'NVO', 'XMTR', 'AMD', 'DIA', 'TSLA', 'SPGI', 'SQM', 'PSTG', 'QUBT', 
-    'JNJ', 'MSTR', 'ENB', 'JPM', 'AFRM', 'VTWG', 'BLOCK', 'BN', 'PLTR', 'NVDA', 'META', 'SITM', 
-    'MARA', 'GOOG', 'HOOD', 'BABA', 'DAR', 'SFM', 'AVO', 'SOUN', 'MRK', 'LMND', 'MO', 'ISRG', 
-    'GUSH', 'VOO', 'CRM', 'EPD', 'UBER', 'DOW', 'AFRM', 'MSFT', 'TSM', 'NFLX', 'LCID', 'IONQ', 
-    'SIRI', 'PGR', 'COST', 'RGTI', 'DDS', 'EQIX', 'FTNT', 'URI', 'ETSY', 'CNQ', 'CVS'
+    'SPY'
 ])
 
 # Button to fetch and process data
