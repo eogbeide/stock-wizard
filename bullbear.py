447cc629cef26a5aa90d37eefda73f5cc73cb516
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-02-25 04:08:56 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -49,7 +49,7 @@ ticker = st.selectbox("Select Stock Ticker:", options=[
 # Button to fetch and process data
 if st.button("Forecast"):
     # Step 1: Download historical data from Yahoo Finance
-    start_date = '2018-01-01'
+    start_date = '2022-01-01'
     end_date = pd.to_datetime("today")
 
     try:
