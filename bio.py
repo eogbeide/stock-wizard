329af0822f2b3373a6034c728f5af5199df3603c
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-08-27 18:58:36 -0700

    Update bio.py

--- bio.py
+++ bio.py
@@ -16,7 +16,7 @@ except Exception:
 
 # ---------- App config ----------
 st.set_page_config(page_title="📖 MCQs + Answers Reader (GitHub → TTS)", page_icon="🎧", layout="wide")
-DEFAULT_URL = "https://raw.githubusercontent.com/eogbeide/stock-wizard/main/biohbooks.docx"
+DEFAULT_URL = "https://raw.githubusercontent.com/eogbeide/stock-wizard/main/biobooks.docx"
 
 # ---------- Helpers ----------
 @st.cache_data(show_spinner=False)
