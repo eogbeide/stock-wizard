6b50281952e89b6db0c6f2a0015370feb4e83615
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-10-26 17:36:50 -0700

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -282,7 +282,7 @@ if mode == "Stock":
 else:
     universe = [
         'EURUSD=X','EURJPY=X','GBPUSD=X','USDJPY=X','AUDUSD=X','NZDUSD=X',
-        'HKDJPY=X','USDCAD=X','USDCNY=X','USDCHF=X','EURGBP=X',
+        'HKDJPY=X','USDCAD=X','USDCNY=X','USDCHF=X','EURGBP=X','HKDJPY=X',
         'USDHKD=X','EURHKD=X','GBPHKD=X','GBPJPY=X','EURCAD=X'
     ]
 
