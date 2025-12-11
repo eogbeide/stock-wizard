acf614d3236f330c3ab670a0ad4045f4814348a6
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-12-11 06:16:20 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -25,7 +25,7 @@
 #                    "Daily — HMA Cross + Trend Agreement (latest bar)".
 #   • NEW (Daily): HMA Cross colors → Buy = Black, Sell = Blue (badges and stars).
 
-import streamlit as st
+import streamlit as std
 import pandas as pd
 import numpy as np
 import yfinance as yf
