774bf150c486726fefb80876604ea4f9abec000d
eogbeide <50998708+eogbeide@users.noreply.github.com>
2025-12-25 22:02:39 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -17,7 +17,7 @@
 #   • NEW: Slope reversal probability for daily & hourly price charts.
 #   • Green dashed global trendline restored on daily & hourly price charts.
 
-import streamlit as st
+import streamlit as stf
 import pandas as pd
 import numpy as np
 import yfinance as yf
