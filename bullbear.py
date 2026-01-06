f43a81cff9d4fc73d7b0a490f10f88f9357ebee1
eogbeide <50998708+eogbeide@users.noreply.github.com>
2026-01-06 10:22:45 -0800

    Update bullbear.py

--- bullbear.py
+++ bullbear.py
@@ -1,7 +1,7 @@
 # =========================
 # Part 1/10 — bullbear.py
 # =========================
-import streamlit as st
+import streamlit as stx
 import pandas as pd
 import numpy as np
 import yfinance as yf
