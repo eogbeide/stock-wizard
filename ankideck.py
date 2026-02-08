23fa2b0063e9fced4f79d3ce9ab1a748fc894e83
eogbeide <50998708+eogbeide@users.noreply.github.com>
2026-02-08 08:03:31 -0800

    Update ankideck.py

--- ankideck.py
+++ ankideck.py
@@ -14,7 +14,7 @@
 #
 DEFAULT_URL = "https://raw.githubusercontent.com/eogbeide/stock-wizard/main/ankideck.pdf"
 
-import io
+import iog
 import re
 import uuid
 from typing import Optional, List, Dict, Tuple
